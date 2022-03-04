729957b057da6cc2560f5baac77426bed8c51003
Roy Nitulescu <roy.nitulescu@gmail.com>
2022-03-04 16:57:29 -0500

    Added overall package doc draft

---
+++ R/package_doc.R
@@ -0,0 +1,17 @@
+#' A package for efficient data summarization using data.table
+#'
+#' The munge package provides the main workhorse function \code{munge}, which
+#' takes care of all the data summarizing automatically, as long as the input data
+#' are properly formatted. The package will be extended to add functions for formating
+#' the default output further for publication-quality output in various file formats (tex, html).
+#' The package also provides a few other utility functions useful for data investigations such as
+#' \code{checkDups}.
+# 
+# @section munge functions:
+# The munge functions ...
+#
+#' @docType package
+#' @name munge-package
+NULL
+#> NULL
+
